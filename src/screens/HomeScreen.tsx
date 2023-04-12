247f67dc3c24df9b8e10322341d8a6eb2b4ab74f
AleCampo86 <alejandrocampo30@gmail.com>
2023-04-12 12:59:48 -0300

    restaurants by location works

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,12 +1,19 @@
+import {useNavigation} from '@react-navigation/native';
 import React from 'react';
 import {View, StyleSheet, Text} from 'react-native';
+import {TouchableOpacity} from 'react-native-gesture-handler';
 import MapView, {Marker} from 'react-native-maps';
 
 const MapScreen: React.FC = () => {
+  const navigation = useNavigation();
   return (
     <View style={styles.container}>
       <View style={styles.searchContainer}>
         <Text style={styles.title}>Restaurant Challenge</Text>
+
+        <TouchableOpacity onPress={() => navigation.navigate('Detail')}>
+          <Text style={styles.location}>Look for Restaurants in your area</Text>
+        </TouchableOpacity>
       </View>
       <View style={styles.mapContainer}>
         <MapView
@@ -52,6 +59,11 @@ const styles = StyleSheet.create({
   title: {
     color: 'black',
     fontSize: 24,
+    textAlign: 'center',
+  },
+  location: {
+    color: 'blue',
+    fontSize: 20,
     textAlign: 'center',
   },
 });
