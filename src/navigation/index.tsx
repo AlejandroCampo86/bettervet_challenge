be885b987db75c1b74f2987e1af4c055c7d831bd
AleCampo86 <alejandrocampo30@gmail.com>
2023-04-14 13:23:32 -0300

    bottom tabs icons ok

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -5,11 +5,12 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {createDrawerNavigator} from '@react-navigation/drawer';
 import DrawerContent from '../../src/components/DrawerContent';
 import {LocalRestaurantsStackScreen, RestaurantsStackScreen} from './stack';
+import Icon from 'react-native-vector-icons/FontAwesome'; // Import Icon from 'react-native-vector-icons/FontAwesome' for icons
 
 // Define a bottom tab navigator type
 type BottomTabParamList = {
-  Tab1: undefined;
-  Tab2: undefined;
+  LocalRestaurantsTab: undefined;
+  RestaurantsTab: undefined;
 };
 
 // Define a drawer navigator type
@@ -27,14 +28,30 @@ const Navigation: React.FC = () => {
       <Drawer.Navigator drawerContent={props => <DrawerContent {...props} />}>
         <Drawer.Screen name="BetterVet Challenge">
           {() => (
-            <BottomTab.Navigator screenOptions={{headerShown: false}}>
+            <BottomTab.Navigator
+              screenOptions={({route}) => ({
+                headerShown: false,
+                tabBarIcon: ({color, size}) => {
+                  // Define icons for each tab based on route name
+                  let iconName;
+                  if (route.name === 'LocalRestaurantsTab') {
+                    iconName = 'cutlery';
+                  } else if (route.name === 'RestaurantsTab') {
+                    iconName = 'glass';
+                  }
+                  // Return the icon component
+                  return <Icon name={iconName} size={size} color={color} />;
+                },
+              })}>
               <BottomTab.Screen
                 name="LocalRestaurantsTab"
                 component={LocalRestaurantsStackScreen}
+                options={{tabBarLabel: 'Local Restaurants'}} // Update tab label
               />
               <BottomTab.Screen
                 name="RestaurantsTab"
                 component={RestaurantsStackScreen}
+                options={{tabBarLabel: 'Restaurants'}} // Update tab label
               />
             </BottomTab.Navigator>
           )}
