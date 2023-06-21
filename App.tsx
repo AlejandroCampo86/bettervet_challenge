5cb36ca0f910b030ee8a772e5ce0a9810a62cb3c
Alecampo86 <alejandrocampo_86@hotmail.com>
2023-06-21 18:19:47 -0300

    typescript fix

--- App.tsx
+++ App.tsx
@@ -1,22 +1,9 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
-import RestaurantsComponent from './src/screens/RestaurantsComponent';
-import LocalRestaurantsComponent from './src/screens/LocalRestaurantsComponent';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {LocationContextProvider} from './src/services/location.context';
 import Navigation from './src/navigation';
 
-// Define a stack navigator type
-type RootStackParamList = {
-  Home: undefined;
-  Detail: {itemId: number};
-};
-
-const Stack = createStackNavigator<RootStackParamList>();
-
-// Define a functional component using TypeScript
 const App: React.FC = () => {
   return (
     <SafeAreaProvider>
